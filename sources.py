1ab41eee7420357bc74de6f1dd248b836464a828
sahil dadia <sdadia@cameramatics.com>
2022-05-17 09:39:54 +0100

    Formatted sources.py

--- sources.py
+++ sources.py
@@ -47,7 +47,7 @@ class Source(ABC):
         # df.index = pd.to_datetime(df.index)
 
         fig, ax = plt.subplots()
-        df.plot(ax=ax, kind="line", y="Close", color='#003366')
+        df.plot(ax=ax, kind="line", y="Close", color="#003366")
         # mpl.plot(df, type="line", style="sas", ax=ax, linecolor="#003366")
 
         ax.set_title(
@@ -97,39 +97,49 @@ class Source(ABC):
     def plotGlobalEvents(self, df, fig, ax):
 
         globalEvents = [
-            {'eventName': 'Ukraine war',
-             'eventDate': '2022-02-24'},
-            {'eventName': 'US sanctions',
-             'eventDate': '2022-03-15'},
-            {'eventName': 'Russian gas in Rubles',
-             'eventDate': '2022-03-22'},
+            {"eventName": "Ukraine war", "eventDate": "2022-02-24"},
+            {"eventName": "US sanctions", "eventDate": "2022-03-15"},
+            {"eventName": "Russian gas in Rubles", "eventDate": "2022-03-22"},
         ]
         globalEventsDF = pd.DataFrame(globalEvents)
         globalEventsDF.eventDate = pd.to_datetime(globalEventsDF.eventDate)
-        globalEventsDF.set_index('eventDate', inplace=True)
+        globalEventsDF.set_index("eventDate", inplace=True)
 
-        mergedEventsDF = pd.merge(left=globalEventsDF, right=df, left_index=True, right_index=True, how='outer')
-        mergedEventsDF = mergedEventsDF[(mergedEventsDF.index >= df.index.min()) & (mergedEventsDF.index <= df.index.max())]
+        mergedEventsDF = pd.merge(
+            left=globalEventsDF,
+            right=df,
+            left_index=True,
+            right_index=True,
+            how="outer",
+        )
+        mergedEventsDF = mergedEventsDF[
+            (mergedEventsDF.index >= df.index.min())
+            & (mergedEventsDF.index <= df.index.max())
+        ]
         if mergedEventsDF.empty:
             return fig, ax
-        mergedEventsDF.interpolate(method='linear', inplace=True, limit_direction='both')
+        mergedEventsDF.interpolate(
+            method="linear", inplace=True, limit_direction="both"
+        )
         # Filter rows where we have events
         mergedEventsDF = mergedEventsDF[mergedEventsDF.eventName.notnull()]
 
         print(mergedEventsDF.info())
         print(mergedEventsDF.head(10))
 
-        ax.scatter(x=mergedEventsDF.index, y=mergedEventsDF.Close, marker='o')
+        ax.scatter(x=mergedEventsDF.index, y=mergedEventsDF.Close, marker="o")
 
         texts = []
         for index, row in mergedEventsDF.iterrows():
-            texts.append(ax.text(index, row['Close'], row['eventName'])
-                         )
-        adjust_text(texts, arrowprops=dict(arrowstyle='->', color='blue'), ax=ax,
-                    expand_points=(2,2),
-                    # expand_text=(3,3),
-                    expand_objects=(3,6),
-                    expand_align=(5,5)
-                    )
+            texts.append(ax.text(index, row["Close"], row["eventName"]))
+        adjust_text(
+            texts,
+            arrowprops=dict(arrowstyle="->", color="blue"),
+            ax=ax,
+            expand_points=(2, 2),
+            # expand_text=(3,3),
+            expand_objects=(3, 6),
+            expand_align=(5, 5),
+        )
 
         return fig, ax
