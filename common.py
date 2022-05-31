da0af5be87394d642fac7a3287e470e69f9b40bd
sahil <sdadia@cameramatics.com>
2022-05-31 21:21:21 +0100

    Added common file

--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import dotenv
 from prompt_toolkit.history import FileHistory
 from rich import console
@@ -9,3 +11,28 @@ console = console.Console()
 session = PromptSession(
     enable_history_search=True, history=FileHistory(".session_history")
 )
+
+
+def findFutureValue(
+    principle: int,
+    numYears: int,
+    rateOfCompounding: float,
+    compoundingFreq: Literal[
+        "daily", "monthly", "quaterly", "semiannually", "annually"
+    ],
+) -> float:
+
+    compoundingFreqMapping = dict(zip([
+        "daily", "monthly", "quaterly", "semiannually", "annually"
+    ],
+    [365, 12, 4, 2, 1]))
+    M = rateOfCompounding/compoundingFreqMapping[compoundingFreq]
+    print(M)
+
+    finalValue: float = principle * (1+(rateOfCompounding*0.01)/M)**(M*numYears)
+    return finalValue
+
+
+
+ans = findFutureValue(10e3, 2, rateOfCompounding=8.0, compoundingFreq='quaterly')
+print(f'{ans:,}')
